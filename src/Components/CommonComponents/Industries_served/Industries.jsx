efe17f4f323074a7f10d38ca7a97122106628592
Reshabcsst <reshabn.csst@gmail.com>
2024-02-22 11:56:00 +0530

    uno

--- src/Components/CommonComponents/Industries_served/Industries.jsx
+++ src/Components/CommonComponents/Industries_served/Industries.jsx
@@ -1,40 +1,34 @@
 import color from '../../assets/img/Rectangle 47.png';
-import truck from '../../assets/Industries_truck.png';
-import health from '../../assets/Industries_health.png';
-import phone from '../../assets/Industries_phone.png';
-import man from '../../assets/Industries_man.png';
-import cart from '../../assets/Industries_cart.png';
-import computer from '../../assets/Industries_computer.png';
 import './Style.css';
 import React from 'react';
 
 const Industries = () => {
   const Industries = [
     {
-        Pic: health,
-        name: 'Healthcare'
+      symbol: <span class="material-symbols-outlined">ecg_heart</span>,
+      name: 'Healthcare'
     },
     {
-        Pic: truck,
-        name: 'Transport & Logistics'
+      symbol: <span class="material-symbols-outlined">local_shipping</span>,
+      name: 'Transport & Logistics'
     },
     {
-        Pic: man,
-        name: 'Food & Beverages'
+      symbol: <span class="material-symbols-outlined">lunch_dining</span>,
+      name: 'Food & Beverages'
     },
     {
-        Pic: phone,
-        name: 'Education & E-learning'
+      symbol: <span class="material-symbols-outlined">school</span>,
+      name: 'Education & E-learning'
     },
     {
-        Pic: cart,
-        name: 'E-Commerce'
+      symbol: <span class="material-symbols-outlined">shopping_bag</span>,
+      name: 'E-Commerce'
     },
     {
-        Pic: computer,
-        name: 'Real Estate'
+      symbol: <span class="material-symbols-outlined"> real_estate_agent</span>,
+      name: 'Real Estate'
     }
-]
+  ]
   return (
     <div className='industries'>
       <div className="upr">
@@ -46,7 +40,8 @@ const Industries = () => {
         {Industries.map((data, index) => {
           return (
             <div key={index} className='list'>
-              <img className='upr1' src={data.Pic} alt="" />
+              {/* <img className='upr1' src={data.Pic} alt="" /> */}
+              <span>{data.symbol}</span>
               <p>{data.name}</p>
             </div>
           )
